9c893fb80ee3b3df2b0d4451b8b1d253319fb5a5
Lucas Pessoa <lucas.pessoa@bosal.com>
2024-04-19 11:30:50 -0300

    Atualizado api.js, app, index.js e index.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,37 @@
+import API from "./serviços/api";
+import {useState} from "react";
+import { IoIosSearch } from "react-icons/io";
 
+export const App = () => {
 
-function App() {
-  return (
-    <div className="Corpo">
-      <h1>Olá Mundo</h1>
-    </div>
-  );
+  const [input, setInput] = useState();
+  const [cidade, setCidade] = useState({});
+
+  async function enviandoDados(){
+
+    if(input === ""){
+      alert("Por favor, preencha o campo abaixo!");
+    }
+
+    try{
+      const resposta = await API.get(`${input}/json`);
+      setCidade(resposta.data);
+    } catch {
+      alert("Erro ao buscar a cidade! Verifique novamente.");
+      setInput("");
+    }
+  }
+
+  return(
+    <>
+      <div>
+
+        <input className="pesquisa" placeholder="Informe sua cidade: " value={input} onChange={(e) => setInput(e.target.value)}></input>
+        <button className="botaoPesquisa" onClick={enviandoDados}><IoIosSearch color="white" size={30}/></button>
+
+      </div>
+    </>
+  )
 }
 
 export default App;
