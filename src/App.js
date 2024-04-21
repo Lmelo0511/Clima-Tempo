a70a2061d88a77f7be2176fb05d01a49af834404
Lucas Melo <lucasmelo@MacBook-Air-de-Lucas.local>
2024-04-21 19:31:02 -0300

    Atualizado index.css, app.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,30 @@
-import API from "./serviços/api";
 import {useState} from "react";
 import { IoIosSearch } from "react-icons/io";
+import axios from "axios"
 
 export const App = () => {
 
   const [input, setInput] = useState();
-  const [cidade, setCidade] = useState({});
+  
+  async function enviandoDados(e){
+    
+    const chaveURL = "25ef575f9e153a5836e6ca22640a992d"
 
-  async function enviandoDados(){
+    const API = axios.create({
+
+      baseURL: `https://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&appid=${chaveURL}&lang=pt_br`
+    
+    });
 
     if(input === ""){
       alert("Por favor, preencha o campo abaixo!");
+      e.preventDefault();
+      return;
     }
-
+    
     try{
-      const resposta = await API.get(`${input}/json`);
-      console.log(resposta);
+      const resposta = await API.get();
+      console.log(resposta.data);
     } catch {
       alert("Erro ao buscar a cidade! Verifique novamente.");
       setInput("");
@@ -25,10 +34,11 @@ export const App = () => {
   return(
     <>
       <div className="preencherCidade">
-
-        <input className="pesquisa" placeholder="Informe sua cidade: " value={input} onChange={(e) => setInput(e.target.value)}></input>
-        <button className="botaoPesquisa" onClick={enviandoDados}><IoIosSearch color="white" size={30}/></button>
-
+        <h1 className="titulo">ClimaTempo</h1>
+        <div className="inputeEPesquisa">
+          <input className="pesquisa" placeholder="Informe sua cidade: " value={input} onChange={(e) => setInput(e.target.value)}></input>
+          <button className="botaoPesquisa" onClick={enviandoDados}><IoIosSearch color="white" size={30}/></button>
+        </div>
       </div>
     </>
   )
