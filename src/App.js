4ac2aaf6d743c1a411d6781df97df01ca6dad9b6
Lucas Pessoa <lucas.pessoa@bosal.com>
2024-04-22 16:48:11 -0300

    Alterado index.css e App.js e inclusão de 3 imagens

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import {useState} from "react";
 import { IoIosSearch } from "react-icons/io";
 import axios from "axios"
+import { WiHumidity } from "react-icons/wi";
+import { FaRegEye } from "react-icons/fa";
 
 export const App = () => {
 
@@ -24,7 +26,7 @@ export const App = () => {
     
     try{
       const resposta = await API.get();
-      console.log(resposta.data);
+      console.log(resposta);
     } catch {
       alert("Erro ao buscar a cidade! Verifique novamente.");
       setInput("");
@@ -34,10 +36,40 @@ export const App = () => {
   return(
     <>
       <div className="preencherCidade">
-        <h1 className="titulo">ClimaTempo</h1>
         <div className="inputeEPesquisa">
           <input className="pesquisa" placeholder="Informe sua cidade: " value={input} onChange={(e) => setInput(e.target.value)}></input>
           <button className="botaoPesquisa" onClick={enviandoDados}><IoIosSearch color="white" size={30}/></button>
+        </div>
+        <br></br>
+        <div className="widgetInformacao1">
+          <span>Brasil - São Paulo</span>
+          <span className="Temperatura">25°</span>
+          <span>Nublado</span>
+          <div className="maxEMin">
+            <span>Máx.: 26</span>
+            <span>Min.: 22</span>
+          </div>
+        </div>
+        <br></br>
+        <div className="umidadeEVento">
+          <div className="umidade">
+            <span><WiHumidity color="white" size={17}/>Umidade:</span>
+            <span className="unidadeMedida">71%</span>
+          </div>
+          <div className="vento">
+            <span>0.76 km/h Vento</span>
+            <hr></hr>
+            <span>1.42 km/h Rajadas</span>
+          </div>
+        </div>
+        <br></br>
+        <div className="visibilidade">
+          <span><FaRegEye size={17} color="white"/>Visibilidade: </span>
+          <span className="unidadeMedida2">10000</span>
+          <span>Pressão: </span>
+        </div>
+        <div className="pressao">
+
         </div>
       </div>
     </>
