aa0dd59505c7b1c39cd0d3ed56bc950e59ef14f7
Lucas Pessoa <lucas.pessoa@bosal.com>
2024-04-29 14:15:48 -0300

    Atualizado ContextoTema.js

--- src/horario/ContextoTema.js
+++ src/horario/ContextoTema.js
@@ -23,7 +23,7 @@ export const ProvedorTema = ({children}) => {
         backgroundAttachment: "fixed",
         backgroundRepeat: "no-repeat",
         borderRadius: "10px",
-        height: "132vh",
+        height: "auto",
     };
 
     return (
